cf2df7fcfc24f6d84a19bfeee2d50d504acc8422
eggs <jonathan.eggiman@gmail.com>
2013-09-15 23:20:09 -0700

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lunchbox::Application.config.secret_key_base = '4d738d921ab383b4bb86d2969f058e1652b2e4ce09533feed0f5e741e89779b8f8f6884b3e245b0f9fca013e0922177ff091b7f63e9bfa619d5ec08330069046'
